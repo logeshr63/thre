129e9a45908da0e2f5dd9f8db237f9bdda327d04
logeshr63 <logeshr63@gmail.com>
2016-07-10 11:41:54 +0530

    Update cirr.c

--- cirr.c
+++ cirr.c
@@ -2,13 +2,13 @@
 #include<stdio.h>
 void main()
 {
-int a,p,i,j,count=0;
+int a,b,i,j,count=0;
 printf("\n Enter the snumber of students:");
 scanf("%d",&a);
-p=2*a;
+b=2*a;
 for(i=0;i<p;i++)
 {
-for(j=i+1;j<=p;j++)
+for(j=i+1;j<=b;j++)
  {
  printf("\n%d\t%d\n",i,j);
  count++;
